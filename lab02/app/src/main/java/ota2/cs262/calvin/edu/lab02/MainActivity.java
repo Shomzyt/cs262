434f3b8df680ad5d1a59e29ebb2686721758110a
Oshomah Agbugui <ota2@maroon07.cs.calvin.edu>
2018-09-21 14:44:54 -0400

    CS 262 labs

---
+++ lab02/app/src/main/java/ota2/cs262/calvin/edu/lab02/MainActivity.java
@@ -0,0 +1,13 @@
+package ota2.cs262.calvin.edu.lab02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
