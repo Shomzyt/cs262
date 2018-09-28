8d4480d03703234c394f14064d58dc9fb8e28a30
Oshomah Agbugui <ota2@maroon11.cs.calvin.edu>
2018-09-28 19:18:07 -0400

    lab04 complete. Homework01 not finished. All labs done.

---
+++ Homework01/app/src/main/java/ota2/cs262/calvin/edu/homework01/MainActivity.java
@@ -0,0 +1,13 @@
+package ota2.cs262.calvin.edu.homework01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
