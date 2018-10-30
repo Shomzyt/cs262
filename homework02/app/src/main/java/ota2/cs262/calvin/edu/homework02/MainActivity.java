75f3453fdfbf3ee6f0435412dcf90feb81f09d56
Oshomah Agbugui <ota2@gold34.cs.calvin.edu>
2018-10-30 19:36:35 -0400

    a concise, informative message

--- homework02/app/src/main/java/ota2/cs262/calvin/edu/homework02/MainActivity.java
+++ homework02/app/src/main/java/ota2/cs262/calvin/edu/homework02/MainActivity.java
@@ -1,52 +1,46 @@
 package ota2.cs262.calvin.edu.homework02;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //    TextView mTitleText;
+//    TextView mAuthorText;
+    EditText idInput;
+    TextView playerList;
+    getMonopolyData theData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        mTitleText = findViewById(R.id.titleText);
+//        mAuthorText = findViewById(R.id.authorText);
+        idInput = findViewById(R.id.IDinput);
+        playerList = findViewById(R.id.playerList);
+        theData = new getMonopolyData(playerList, idInput, this);
+//
+//        if(getSupportLoaderManager().getLoader(0)!=null){
+//            getSupportLoaderManager().initLoader(0,null,this);
+//        }
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void fetchData(View view) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (theData != null) {
+            theData.cancel(true);
         }
+        theData = new getMonopolyData(playerList, idInput, this);
+        theData.execute();
 
-        return super.onOptionsItemSelected(item);
     }
 }
